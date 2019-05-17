fefc4251404ea753f42759eba837097a3eefe510
Drunella <48769805+Drunella@users.noreply.github.com>
2019-05-18 00:21:24 +0100

    started with d81 support

--- Makefile
+++ Makefile
@@ -70,7 +70,7 @@ build/ef/loader.prg: $(LOADER_FILES)
 	$(LD65) $(LD65FLAGS) -vm -m ./build/ef/loader.map -o $@ -C src/ef/loader.cfg c64.lib $(LOADER_FILES)
 
 # music
-build/ef/music.prg build/files/music_rom.bin build/ef/music.map: $(MUSIC_FILES)
+build/ef/music.prg build/ef.f/music_rom.bin build/ef/music.map: $(MUSIC_FILES)
 	$(LD65) $(LD65FLAGS) -vm -m ./build/ef/music.map -o build/ef/music.prg -C src/ef/music.cfg $(MUSIC_FILES)
 
 # music map
@@ -97,13 +97,6 @@ build/ef/transfer-load.prg build/ef/transfer-load.map: build/ef/transfer-load.o
 #build/ef/transfer-load.inc: build/ef/transfer-load.map
 #	tools/parsemap.py -v -s ./build/ef/transfer-load.map -d build/ef/transfer-load.inc -e _disk_load_block -e _disk_check_type
 
-# source files
-build/source/files.list:
-	tools/extract.py -v -d ./src/disks.cfg -s ./disks -b ./build/source
-
-# get m.prg	
-build/source/m.prg:
-	c1541 disks/osi.d64 -read m ./build/source/m.prg
 
 # disassemble m.prg 
 build/ef/music-disassemble.o: build/source/m.prg src/ef/music-disassemble.info ./src/ef/music-export.i
@@ -112,34 +105,31 @@ build/ef/music-disassemble.o: build/source/m.prg src/ef/music-disassemble.info .
 	$(CA65) $(CA65FLAGS) -o ./build/ef/music-disassemble.o ./build/temp/music-disassemble.s
 
 # files with additional items
-build/files/files.list: build/source/files.list build/ef/io-addendum.prg build/ef/music.prg
-	cp ./build/source/* ./build/files/
-	cp build/ef/io-addendum.prg build/files/io.add.prg
-	cp build/ef/music.prg build/files/music.prg
-	echo "0x41/io.add io.add" >> build/files/files.list
-	echo "0x41/music music" >> build/files/files.list
+build/ef.f/files.list: build/source/files.list build/ef/io-addendum.prg build/ef/music.prg
+	cp ./build/source/* ./build/ef.f/
+	cp build/ef/io-addendum.prg build/ef.f/io.add.prg
+	cp build/ef/music.prg build/ef.f/music.prg
+	echo "0x41/io.add io.add" >> build/ef.f/files.list
+	echo "0x41/music music" >> build/ef.f/files.list
 	
 # patch
-build/files/patched.done: build/files/files.list build/ef/io-replacement.map build/ef/transfer-load.map build/ef/music.map build/ef/transfer-load.prg build/files/music_rom.bin
-	#tools/mkpatch_tempsubs.sh ./build/patches ./build/ef/io-replacement.inc ./build/ef/music.inc
-	#tools/mkpatch_transfer.sh ./build/patches ./build/ef/transfer-load.inc
-	#tools/mkpatch_music.sh ./build/patches ./build/ef/music.inc
-	tools/u5patch.py -v -l ./build/files/files.list -f ./build/files -m build/ef/io-replacement.map -m build/ef/transfer-load.map -m build/ef/music.map ./patches/*.patch
-	cp build/files/music_rom.bin build/ef/music_rom.aprg
-	touch ./build/files/patched.done
+build/ef.f/patched.done: build/ef.f/files.list build/ef/io-replacement.map build/ef/transfer-load.map build/ef/music.map build/ef/transfer-load.prg build/ef.f/music_rom.bin
+	tools/u5patch.py -v -l ./build/ef.f/files.list -f ./build/ef.f -m build/ef/io-replacement.map -m build/ef/transfer-load.map -m build/ef/music.map ./patches/ef/*.patch
+	cp build/ef.f/music_rom.bin build/ef/music_rom.aprg
+	touch ./build/ef.f/patched.done
 	 
 # crunch
-build/files/crunched.done: build/files/patched.done
-	tools/crunch.py -v -b ./build/files
-	touch build/files/crunched.done
+build/ef.f/crunched.done: build/ef.f/patched.done
+	tools/crunch.py -v -b ./build/ef.f
+	touch build/ef.f/crunched.done
 
 # build efs
-build/ef/directory.data.prg build/ef/files.data.prg: build/files/crunched.done
-	tools/mkefs.py -v -o ./src/disks.cfg  -x ./src/ef/exclude.cfg -f ./build/files -e crunch -d ./build/ef
+build/ef/directory.data.prg build/ef/files.data.prg: build/ef.f/crunched.done
+	tools/mkefs.py -v -o ./src/disks.cfg  -x ./src/ef/exclude.cfg -f ./build/ef.f -e crunch -d ./build/ef
 
 # build blocks
-build/ef/crt.blocks.map: build/files/files.list
-	tools/mkblocks.py -v -o ./src/disks.cfg -b ./src/ef/block.map -f ./build/files -m ./build/ef/crt.blocks.map -d ./build/ef
+build/ef/crt.blocks.map: build/ef.f/files.list
+	tools/mkblocks.py -v -o ./src/disks.cfg -b ./src/ef/block.map -f ./build/ef.f -m ./build/ef/crt.blocks.map -d ./build/ef
 
 # cartridge binary
 build/ef/u5remastered.bin: build/ef/directory.data.prg build/ef/files.data.prg build/ef/exodecrunch.prg build/ef/init.prg build/ef/loader.prg src/ef/eapi-am29f040.prg build/ef/crt.blocks.map build/ef/music_rom.aprg
@@ -152,48 +142,43 @@ build/u5remastered.crt: build/ef/u5remastered.bin
 	cartconv -b -t easy -o build/u5remastered.crt -i build/ef/u5remastered.bin -n "Ultima 5 Remastered" -p
 
 
+# d81
+# io-replacement d81
+build/d81/io-replacement.prg build/d81/io-replacement.map: build/d81/io-code.o build/exo/exodecrunch.o
+	$(LD65) $(LD65FLAGS) -vm -m ./build/d81/io-replacement.map -o build/d81/io-replacement.prg -C ./src/d81/io-replacement.cfg $^
+
+
 subdirs:
-	@mkdir -p ./build/temp ./build/exo
+	@mkdir -p ./build/temp 
+	@mkdir -p ./build/exo
+	@mkdir -p ./build/d81.f
+	@mkdir -p ./build/d81
 	@mkdir -p ./build/source
-	@mkdir -p ./build/files
-	@mkdir -p ./build/patches
+	@mkdir -p ./build/ef.f
 	@mkdir -p ./build/ef
 
 clean:
 	rm -rf build/ef
 	rm -rf build/d81
-	rm -rf build/files
-	rm -rf build/patches
+	rm -rf build/ef.f 
+	rm -rf build/d81.f
 	rm -rf build/temp
 	rm -rf build/exo
 	rm -f build/u5remastered.crt
+	rm -f build/u5remastered.d81
 
 mrproper:
 	rm -rf build
 
 
-#
-#build/ef/src/io/data_loader.exported.inc: src/io/data_loader.map
-#	tools/parsemap.py -v -s ./src/io/data_loader.map -d ./src/io/data_loader.exported.inc -e get_crunched_byte -e load_prg -e save_prg_byte -e save_source_high -e save_source_low -e load_block -e load_destination_high -e load_destination_low -e save_source_low -e save_source_high
+# source files
+build/source/files.list:
+	tools/extract.py -v -d ./src/disks.cfg -s ./disks -b ./build/source
 
-## data loader
-#build/obj/data_loader.prg src/io/data_loader.map: src/io/data_loader.s
-#	$(CL65) $(CL65FLAGS) -vm -m ./src/io/data_loader.map -o build/obj/data_loader.prg -C src/io/data_loader.cfg $^
-## data loader map
-#src/io/data_loader.exported.inc: src/io/data_loader.map
-#	tools/parsemap.py -v -s ./src/io/data_loader.map -d ./src/io/data_loader.exported.inc -e get_crunched_byte -e load_prg -e save_prg_byte -e save_source_high -e save_source_low -e load_block -e load_destination_high -e load_destination_low -e save_source_low -e save_source_high
+# get m.prg	
+build/source/m.prg:
+	c1541 ./disks/osi.d64 -read m ./build/source/m.prg
 
-# io
-#build/obj/io.prg src/io/io.map: src/io/io.s src/io/data_loader.exported.inc
-#	$(CL65) $(CL65FLAGS) -vm -m ./src/io/io.map -o ./build/obj/io.prg -C ./src/io/io.cfg ./src/io/io.s
-# io.map
-#src/io/io.exported.inc: src/io/io.map
-#	tools/parsemap.py -v -s ./src/io/io.map -d ./src/io/io.exported.inc -e IO_load_file_entry -e IO_read_block_entry -e IO_request_disk_id_entry -e IO_request_disk_char_entry -e IO_save_file_entry -e IO_read_block_alt_entry -e read_block_filename -e requested_disk -e save_files_flags
- 
-# io jump table replacements
-#build/ef/temp.subs.patched.prg: build/ef/io-replacement.inc
-#	c1541 disks/osi.d64 -read temp.subs build/ef/temp.subs.prg
-#	tools/mkpatch_tempsubs.sh ./build/patches ./build/ef/io-replacement.inc
-#	# replace
-#	tools/u5patch.py -v -s ./build/ef/temp.subs.prg -d ./build/ef/temp.subs.patched.prg ./build/patches/temp.subs.*
-
+# get xyzzy.prg	
+build/source/xyzzy.prg:
+	c1541 ./disks/osi.d64 -read xyzzy ./build/source/xyzzy.prg
---
+++ src/d81/io-code.s
@@ -0,0 +1,523 @@
+; ----------------------------------------------------------------------------
+; Copyright 2019 Drunella
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+;     http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; ----------------------------------------------------------------------------
+
+.include "io.i"
+
+; jump table vectors must be changed via patch
+;
+; 6c09: IO_request_disk_id
+;     param:
+;        A: requested disk id in range 0x01 to 0x08
+;     return:
+;        C set: disk not inserted
+;        C clear: disk inserted
+;
+; 6c2a: IO_request_disk_char
+;     param:
+;        A: requested disk id in range 0x41 to 0x48
+;     return:
+;        C set: disk not inserted
+;        C clear: disk inserted
+;
+; 6c24: IO_load_file
+;    param:
+;        filename: after return address, null terminated
+;        x: 0=return; x=1 jmp 0x0800; x>1 jmp 0xa700
+;    return:
+;        none
+;
+; 6c2d: IO_save_file
+;    saves a file. parameter list, deletes the file and saves it then
+;    param:
+;        X: unknown
+;        string: (after return address) null terminated filename, prepended with "S:"
+;        word: (after return address) address
+;        word: (after return address) size
+;    return:
+;        none
+;
+; 6c00: IO_read_block
+;     param:
+;         Y: track of disk
+;         X: sector of disk
+;         A: destination high address
+;     return:
+;         none
+;
+; 6c30: IO_read_block_alt
+;     reads a block and the following block to a fixed address (0x7e, 0x7f)
+;     param:
+;         A: unknown
+;         X: unknown
+;     return:
+;         none
+;
+
+; export the entry points of the functions
+.export _IO_request_disk_id_entry
+.export _IO_request_disk_char_entry
+.export _IO_load_file_entry
+.export _IO_save_file_entry
+.export _IO_read_block_entry
+.export _IO_read_block_alt_entry
+
+.export get_crunched_byte
+.export decrunch_table
+
+; imports
+.import EXO_decrunch
+
+
+.segment "IO_CODE"
+
+    ; --------------------------------------------------------------------
+    _IO_request_disk_id_entry:
+        clc
+        adc #$40   ; add 40 to get the character
+
+    ; --------------------------------------------------------------------
+    _IO_request_disk_char_entry:
+        sta requested_disk
+        clc        ; disk request always succeeds
+        rts
+
+    ; --------------------------------------------------------------------
+    ; IO_load_file_entry: load file
+    ; filename after return address
+    ; x: return mode (0, 1, >1)
+    _IO_load_file_entry:
+        stx requested_loadmode
+
+        ; set fileparameter
+        lda #$08
+        ldx drive_id
+        ldy #$01 
+        jsr kernal_SETLFS
+
+        ; copy name
+        pla
+        sta copy_name_address_low
+        pla
+        sta copy_name_address_high
+        jsr copy_filename ; returns length in A
+
+        ; set filename
+        ldx #<requested_fullname
+        ldy #>requested_fullname
+        jsr kernal_SETNAM  ; A = File name length; X/Y = Pointer to file name.
+
+        ; load
+        jsr $0126  ; sound off
+        lda #$00 
+    :   jsr kernal_LOAD
+        bcs :-     ; repeat on error
+
+        ; ### decrunch if necessary X/Y last loaded byte
+        jsr decrunch_prepare
+     
+        lda #$08
+        jsr kernal_CLOSE
+        jsr $0129  ; sound on
+
+        ; decide how to start (if) te loaded prg
+        lda requested_loadmode
+        beq load_return                ; 0: return
+        cmp #$01
+        beq load_jumptomain            ; 1: jump to $800
+        jmp $a700                      ; >1: jump to $a700
+    load_jumptomain:
+        jmp $8000
+    load_return:
+        lda copy_name_address_high    ; return address on stack
+        pha
+        lda copy_name_address_low
+        pha
+        rts
+    requested_loadmode:
+        .byte $00
+
+
+    ; --------------------------------------------------------------------
+    ; IO_save_file_entry
+    ; read parameter from after return address
+    ; string: (after return address) null terminated filename, prepended with "S:"
+    ; word: (after return address) address
+    ; word: (after return address) size
+    _IO_save_file_entry:
+        jsr $0126  ; sound off
+
+        pla                            ; load return address to copy opcode
+        sta copy_name_address_low
+        pla
+        sta copy_name_address_high
+
+        ; skip over "S:"
+        jsr getnext_name_character
+        jsr getnext_name_character
+
+        ; copy filename
+        jsr copy_filename
+        sta $ff    ; length of filename now in fc
+        clc
+        adc #$02
+        ldx #<requested_deletename
+        ldy #>requested_deletename
+        jsr kernal_SETNAM
+
+        ; delete
+        lda #$0f  
+        ldx drive_id
+        ldy #$0f  
+        jsr kernal_SETLFS
+        jsr kernal_OPEN
+        lda #$0f  
+        jsr kernal_CLOSE
+        
+        ; copy address and size
+        jsr getnext_name_character
+        sta save_source_low
+        jsr getnext_name_character
+        sta save_source_high
+        jsr getnext_name_character
+        sta save_files_size_low
+        jsr getnext_name_character
+        sta save_files_size_high
+
+        ; prepare saving
+        lda #$08  
+        ldx drive_id
+        ldy #$00  
+        jsr kernal_SETLFS
+
+        ; set name
+        lda $ff
+        ldx #<requested_fullname
+        ldy #>requested_fullname
+        jsr kernal_SETNAM
+
+        ; save parameter
+        ; copy address and size
+        jsr getnext_name_character  ; address low
+        sta $fe
+        jsr getnext_name_character  ; address high
+        sta $ff
+        jsr getnext_name_character  ; size low
+        sta $fc
+        jsr getnext_name_character  ; size high
+        sta $fd
+    :   clc
+        lda $fe
+        adc $fc
+        tax
+        lda $ff
+        adc $fd
+        tay
+        lda #$fe
+        jsr kernal_SAVE  ; A = Address of zero page w start address; X/Y = End address of memory + 1
+        bcs :-
+        
+        ; close
+        jsr $0129  ; music on
+        lda #$08  
+        jsr kernal_CLOSE
+
+        ; leave
+        lda copy_name_address_high    ; return address on stack
+        pha
+        lda copy_name_address_low
+        pha
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; meaning of function unclear, copied from temp.subs
+    ; parameter a, x
+    ; some calculations to get the track and number from a
+    _IO_read_block_alt_entry:
+        sta alt_sector
+        sta alt_track
+        txa
+        lsr a
+        ror alt_track
+        lsr alt_track
+        lsr alt_track
+        lda alt_sector
+        and #$07
+        asl a
+        sta alt_sector
+        lda #$7e
+        ldy alt_track
+        ldx alt_sector
+        jsr _IO_read_block_entry
+        lda #$7f
+        ldy alt_track
+        ldx alt_sector
+        inx
+        jmp _IO_read_block_entry
+    alt_track:
+        .byte $00
+    alt_sector:
+        .byte $00
+
+
+    ; --------------------------------------------------------------------
+    ; IO_read_block_entry
+    ; y:track x:sector a:high destination address
+    ; modify track and sector and call original_load_block
+    _IO_read_block_entry:
+        sta $fc
+        lda requested_disk
+        sec
+        sbc #$41
+        asl
+        clc
+        adc #<load_block_jumptable
+        sta $fe
+        lda #$00
+        adc #>load_block_jumptable
+        sta $ff
+        jmp ($fe)
+    load_block_jumptable:
+        .addr disk_dungeon
+        .addr disk_britannia
+        .addr disk_underworld
+        .addr disk_towne
+        .addr disk_dwelling
+        .addr disk_castle
+        .addr disk_keep
+
+    disk_dungeon:  ; sectors 0-15
+        clc
+        tya
+        adc #dungeon_track_correction
+        tay
+        clc
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_britannia:  ; sectors 0-15
+        clc
+        tya
+        adc #britannia_track_correction
+        tay
+        clc
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_underworld:  ; sectors 16-31
+        clc
+        tya
+        adc #underworld_track_correction
+        tay
+        clc
+        txa
+        adc #$10   ; sector correction
+        tax
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_towne:  ; sectors 0-15
+        clc
+        tya
+        adc #towne_track_correction
+        tay
+        clc
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_dwelling:  ; sectors 16-31
+        clc
+        tya
+        adc #dwelling_track_correction
+        tay
+        clc
+        txa
+        adc #$10   ; sector correction
+        tax
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_castle:  ; sectors 0-15
+        clc
+        tya
+        adc #castle_track_correction
+        tay
+        clc
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+    disk_keep:  ; sectors 16-31
+        clc
+        tya
+        adc #keep_track_correction
+        tay
+        clc
+        txa
+        adc #$10   ; sector correction
+        tax
+        lda $fc
+        jmp original_load_block
+        ; no rts
+
+
+    ; ====================================================================
+    ; loading file utility, search in several efs dirs
+    ; uses fe, ff in zeropage
+
+    ; --------------------------------------------------------------------
+    ; returns next character in A
+    getnext_name_character:
+    copy_name_address_low = copy_name_address + 1
+    copy_name_address_high = copy_name_address + 2
+        inc copy_name_address_low   ; first increase address
+        bne copy_name_address
+        inc copy_name_address_high
+    copy_name_address:
+        lda $ffff                            ; then load
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; copies filename to temporary storage
+    ; return length in A
+    copy_filename:
+        ldy #$ff
+    :   iny
+        jsr getnext_name_character     ; next char in A
+        sta requested_filename, y      ; and store
+        bne :-
+        tya
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; checks if decrunch necessary and decrunches
+    ; X/Y address of last byte loaded
+    decrunch_prepare:
+        ; increase address by one byte
+        inx
+        bne :+
+        iny
+    :   stx copy_name_address_low
+        sty copy_name_address_high
+        
+        ; check if decrunch necessary
+        ldx #<name_list
+        ldy #>name_list
+        jsr compare_filename
+        ldx #<name_slist
+        ldy #>name_slist
+        jsr compare_filename
+        ldx #<name_btlist
+        ldy #>name_btlist
+        jsr compare_filename
+        ldx #<name_utlist
+        ldy #>name_utlist
+        jsr compare_filename
+        ldx #<name_roster
+        ldy #>name_roster
+        jsr compare_filename
+        ldx #<name_prtydata
+        ldy #>name_prtydata
+        jsr compare_filename
+
+        ; decrunch
+        jsr EXO_decrunch
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; gets next byte to decrunch, moves backwars
+    ; must not change X, Y, C
+    get_crunched_byte:
+        lda copy_address_low    ; decrease
+        bne :+
+        dec copy_address_high
+    :   dec copy_address_low
+    copy_address_low = * + 1
+    copy_address_high = * + 2
+        lda $ffff               ; load
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; compare filename 
+    ; name address in x,y
+    compare_filename:
+        stx $fe
+        sty $ff
+
+        ; compare filename
+        ldy #$ff
+        sty $fc  ; 0xff means decrunch necessary
+    nameloop:
+        iny
+        lda #$2a   ; '*'
+        cmp requested_fullname, y  ; character in name is '*', we have a match
+        beq namematch
+        lda requested_fullname, y  ; compare character with character in entry
+        cmp ($fe), y               ; if not equal nextname
+        bne nomatch
+        lda #$0                    ; compare if both character are zero
+        cmp ($fe), y               ; if not, next name
+        beq namematch
+        jmp nameloop
+    namematch: ; if match we return double, as no decrnch
+        tsx
+        dex
+        dex
+        txs
+    nomatch:
+        rts
+
+
+    ; --------------------------------------------------------------------
+    ; variables
+    requested_deletename:
+        .byte $53, $3a  ; "S:"
+    requested_fullname:
+    requested_disk:
+        .byte $41
+    requested_filename:
+        .res 15, $00
+
+    save_files_size_low:
+        .byte $ff
+    save_files_size_high:
+        .byte $ff
+    save_source_low:
+        .byte $ff
+    save_source_high:
+        .byte $ff
+
+    name_list:
+        .byte $42, $4c, $49, $53, $54, $00  ; 'BLIST'
+    name_slist:
+        .byte $42, $53, $4c, $49, $53, $54, $00  ; 'BSLIST'
+    name_utlist:
+        .byte $48, $54, $4c, $49, $53, $54, $00  ; 'HTLIST'
+    name_btlist:
+        .byte $42, $54, $4c, $49, $53, $54, $00  ; 'BTLIST'
+    name_roster:
+        .byte $42, $50, $52, $4f, $53, $54, $45, $52, $00  ; 'BROSTER'
+    name_prtydata:
+        .byte $42, $50, $52, $54, $59, $2e, $44, $41, $54, $41, $00  ; 'BPRTY.DATA'
